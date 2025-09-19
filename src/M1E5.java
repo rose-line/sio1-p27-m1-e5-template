c99649c638d10ef36a301bb392d65438c073f9cf
rose-line <1886792+rose-line@users.noreply.github.com>
2025-09-19 21:16:00 +0200

    maj comm

--- src/M1E5.java
+++ src/M1E5.java
@@ -2,6 +2,8 @@ public class M1E5 {
   public static void main(String[] args) throws Exception {
 
     // Entrez votre code ici
+    // Spécifiez les différentes étapes
+    // de l'algorithme par des commentaires
 
 
   }
